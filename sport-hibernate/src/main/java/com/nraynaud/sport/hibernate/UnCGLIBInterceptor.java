64b38383260d1103a4bd2942a9c77a2236a75a12
nraynaud <nraynaud@d6f93abe-8f3f-0410-b366-b1a3be76d8b4>
2007-12-12 23:37:25 +0000

    - better test coverage for DurationConverter

--- sport-hibernate/src/main/java/com/nraynaud/sport/hibernate/UnCGLIBInterceptor.java
+++ sport-hibernate/src/main/java/com/nraynaud/sport/hibernate/UnCGLIBInterceptor.java
@@ -3,6 +3,7 @@ package com.nraynaud.sport.hibernate;
 import org.hibernate.EmptyInterceptor;
 import org.springframework.util.ClassUtils;
 
+//to correct interaction bug between Spring and Hibernate (with such names, everybody would have gessed problems)
 public class UnCGLIBInterceptor extends EmptyInterceptor {
     public String getEntityName(final Object object) {
         return (object == null ? null : ClassUtils.getShortName(object.getClass()));
--- sport-j2ee/src/test/java/com/nraynaud/sport/web/converter/DurationConverterTest.java
+++ sport-j2ee/src/test/java/com/nraynaud/sport/web/converter/DurationConverterTest.java
@@ -8,19 +8,23 @@ import org.junit.Test;
 public class DurationConverterTest {
     @Test
     public void testVariousConvertions() {
-        checkConvertion(10 * 60, "10");
-        checkConvertion(3 * 3600, "3h");
-        checkConvertion(11, "11''");
-        checkConvertion(10 * 60 + 43, "10'43");
-        checkConvertion(3 * 3600 + 10 * 60 + 43, "3h10'43");
-        checkConvertion(3 * 3600 + 10 * 60, "3h10");
+        checkConvertion(0, 2, 0, "2");
+        checkConvertion(3, 0, 0, "3h");
+        checkConvertion(0, 0, 4, "4''");
+        checkConvertion(0, 5, 4, "5'4");
+        checkConvertion(0, 1, 4, "1'4''");
+        checkConvertion(3, 1, 4, "3h1'4");
+        checkConvertion(3, 1, 4, "3h1'4''");
+        checkConvertion(3, 1, 0, "3h1");
+        checkConvertion(3, 1, 0, "3h1'");
+        checkConvertion(34, 15, 46, "34h15'46''");
     }
 
     @Test
     public void testFormat() {
-        checkFormat("10''", 10);
-        checkFormat("23'11''", 23 * 60 + 11);
-        checkFormat("45h23'11''", 45 * 3600 + 23 * 60 + 11);
+        checkFormat("10''", 0, 0, 10);
+        checkFormat("23'11''", 0, 23, 11);
+        checkFormat("45h23'11''", 45, 23, 11);
         assertEquals("", format(null));
     }
 
@@ -34,8 +38,8 @@ public class DurationConverterTest {
         }
     }
 
-    private static void checkFormat(final String expected, final long input) {
-        final String result = format(Long.valueOf(input));
+    private static void checkFormat(final String expected, final int hours, final int minutes, final int seconds) {
+        final String result = format(Long.valueOf(hoursMinutesSeconds(hours, minutes, seconds)));
         assertEquals(expected, result);
     }
 
@@ -44,13 +48,17 @@ public class DurationConverterTest {
         return converter.convertToString(null, in);
     }
 
-    private static void checkConvertion(final long expected, final String input) {
+    private static void checkConvertion(final int hours, final int minutes, final int seconds, final String input) {
         final Object result = convert(input);
-        assertEquals(Long.valueOf(expected), result);
+        assertEquals(Long.valueOf(hoursMinutesSeconds(hours, minutes, seconds)), result);
     }
 
     private static Object convert(final String input) {
         final DurationConverter converter = new DurationConverter();
         return converter.convertFromString(null, new String[]{input}, Long.class);
+    }
+
+    private static long hoursMinutesSeconds(final int hours, final int minutes, final int seconds) {
+        return hours * 3600 + minutes * 60 + seconds;
     }
 }
