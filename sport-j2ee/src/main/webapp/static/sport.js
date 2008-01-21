4637ac43db97207f711b9dd3508b9169a7e7217d
nraynaud <nraynaud@d6f93abe-8f3f-0410-b366-b1a3be76d8b4>
2008-01-21 16:22:49 +0000

    - bug in workout feedback

--- sport-j2ee/src/main/webapp/static/sport.js
+++ sport-j2ee/src/main/webapp/static/sport.js
@@ -2,10 +2,12 @@ var oldValue = ''
 function feedback(field_name, val)
 {
     var feedbackElement = field_name + '_feedback';
-    if (val.length != 0 && oldValue != val) {
-        new Ajax.Updater(feedbackElement, '/feedback',
-        {asynchronous:true, evalScripts:true,
-            parameters:'data=' + val + '&type=' + field_name}).updateContent();
+    if (val.length != 0) {
+        if (oldValue != val) {
+            new Ajax.Updater(feedbackElement, '/feedback',
+            {asynchronous:true, evalScripts:true,
+                parameters:'data=' + val + '&type=' + field_name}).updateContent();
+        }
     } else {
         Element.update(feedbackElement, '');
     }
