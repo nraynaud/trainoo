78b48271abe36ec3439a073cacd03c0bba21446b
nraynaud <nraynaud@d6f93abe-8f3f-0410-b366-b1a3be76d8b4>
2008-02-04 21:51:28 +0000

    - generalized date conversion - layout of the message delete button

--- sport-j2ee/src/main/java/com/nraynaud/sport/web/converter/DateConverter.java
+++ sport-j2ee/src/main/java/com/nraynaud/sport/web/converter/DateConverter.java
@@ -4,48 +4,74 @@ import com.opensymphony.xwork2.util.TypeConversionException;
 import org.apache.struts2.util.StrutsTypeConverter;
 import org.joda.time.DateMidnight;
 import org.joda.time.DateTime;
+import org.joda.time.DateTimeFieldType;
 import org.joda.time.format.DateTimeFormat;
 import org.joda.time.format.DateTimeFormatter;
 
 import java.util.Date;
+import java.util.HashMap;
 import java.util.Map;
 
 public class DateConverter extends StrutsTypeConverter {
-    private static final DateTimeFormatter DATE_FORMATTER = DateTimeFormat.forPattern("dd/MM/yy");
+    private static final DateTimeFormatter DATE_FORMATTER = DateTimeFormat.forPattern("dd/MM/yyyy");
     private static final Parser FULL_FORMAT_PARSER = new Parser() {
         public DateTime parse(final String source) throws IllegalArgumentException {
             return DATE_FORMATTER.parseDateTime(source);
         }
     };
+
     private static final Parser DAY_MOUTH_PARSER = new Parser() {
         public DateTime parse(final String source) throws IllegalArgumentException {
-            final DateTime dateTime = DateTimeFormat.forPattern("dd/MM").parseDateTime(source);
-            return dateTime.withYear(new DateTime().getYear());
-        }
-    };
-    private static final Parser DAY_PARSER = new Parser() {
-        public DateTime parse(final String source) throws IllegalArgumentException {
-            final DateTime dateTime = DateTimeFormat.forPattern("dd").parseDateTime(source);
-            final DateTime now = new DateTime();
-            return dateTime.withYear(now.getYear()).withMonthOfYear(now.getMonthOfYear());
+            return parseAndComplete(source, "dd/MM", DateTimeFieldType.monthOfYear());
         }
     };
 
-    public static final Parser TODAY_PARSER = new Parser() {
+    private static final Parser DAY_PARSER = new Parser() {
+        public DateTime parse(final String source) throws IllegalArgumentException {
+            return parseAndComplete(source, "dd", DateTimeFieldType.dayOfMonth());
+        }
+    };
+
+    private static DateTime parseAndComplete(final String source, final String pattern,
+                                             final DateTimeFieldType fieldType) {
+        return withPastField(DateTimeFormat.forPattern(pattern).parseDateTime(source), now(), fieldType);
+    }
+
+    private static DateTime withPastField(final DateTime dateTime, final DateTime now, final DateTimeFieldType field) {
+        final DateTimeFieldType next = NEXT.get(field);
+        if (next == null)
+            return dateTime;
+        final int thisField = now.get(next);
+        final int fieldValue = dateTime.get(field) > now.get(field) ? thisField - 1 : thisField;
+        return withPastField(dateTime.withField(next, fieldValue), now, next);
+    }
+
+    private static final Map<DateTimeFieldType, DateTimeFieldType> NEXT = new HashMap<DateTimeFieldType, DateTimeFieldType>() {
+        {
+            put(DateTimeFieldType.monthOfYear(), DateTimeFieldType.year());
+            put(DateTimeFieldType.dayOfMonth(), DateTimeFieldType.monthOfYear());
+        }
+    };
+
+    public static final Parser WORD_PARSER = new Parser() {
         public DateTime parse(final String source) throws IllegalArgumentException {
             if (source.length() > 0) {
                 if ("aujourd'hui".startsWith(source.toLowerCase()))
-                    return new DateTime();
+                    return now();
                 if ("hier".startsWith(source.toLowerCase()))
-                    return new DateTime().minusDays(1);
+                    return now().minusDays(1);
                 if ("avant-hier".startsWith(source.toLowerCase()))
-                    return new DateTime().minusDays(2);
+                    return now().minusDays(2);
             }
             throw new IllegalArgumentException();
         }
     };
 
-    private static final Parser[] PARSERS = {FULL_FORMAT_PARSER, DAY_MOUTH_PARSER, DAY_PARSER, TODAY_PARSER};
+    private static final Parser[] PARSERS = {FULL_FORMAT_PARSER, DAY_MOUTH_PARSER, DAY_PARSER, WORD_PARSER};
+
+    private static DateTime now() {
+        return new DateTime();
+    }
 
     @SuppressWarnings({"RawUseOfParameterizedType"})
     public Object convertFromString(final Map context, final String[] values, final Class toClass) {
