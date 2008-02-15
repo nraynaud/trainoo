c4bc0eedb6a52ed1b36bc7bb1a456d0c11a72e5a
nraynaud <nraynaud@d6f93abe-8f3f-0410-b366-b1a3be76d8b4>
2008-02-15 11:39:01 +0000

    - corrected XSS in url parameter

--- sport-j2ee/src/main/java/com/nraynaud/sport/web/view/Helpers.java
+++ sport-j2ee/src/main/java/com/nraynaud/sport/web/view/Helpers.java
@@ -273,7 +273,7 @@ public class Helpers {
 
     private static void pushParam(final StringBuilder url, final String key, final String value) {
         try {
-            url.append(key).append('=').append(URLEncoder.encode(value, "ISO-8859-1"));
+            url.append(URLEncoder.encode(key, "ISO-8859-1")).append('=').append(URLEncoder.encode(value, "ISO-8859-1"));
         } catch (UnsupportedEncodingException e) {
             throw new RuntimeException(e);
         }
