f59b682f1019607fd3b9532fa99472b8542efe46
nraynaud <nraynaud@d6f93abe-8f3f-0410-b366-b1a3be76d8b4>
2008-04-09 09:20:48 +0000

    - added geoportail tiles client-side headers

--- sport-j2ee/src/main/java/com/nraynaud/sport/web/action/track/GetTileAction.java
+++ sport-j2ee/src/main/java/com/nraynaud/sport/web/action/track/GetTileAction.java
@@ -4,24 +4,35 @@ import com.nraynaud.sport.Application;
 import com.nraynaud.sport.geography.TileFetcher;
 import com.nraynaud.sport.web.actionsupport.DefaultAction;
 import org.apache.struts2.config.Result;
+import org.apache.struts2.config.Results;
+import org.apache.struts2.dispatcher.HttpHeaderResult;
 import org.apache.struts2.dispatcher.StreamResult;
+import org.apache.struts2.interceptor.ServletRequestAware;
 import org.apache.struts2.interceptor.ServletResponseAware;
 import org.apache.struts2.interceptor.validation.SkipValidation;
 import org.joda.time.DateTime;
 import org.joda.time.format.DateTimeFormat;
+import org.joda.time.format.DateTimeFormatter;
 
+import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.ByteArrayInputStream;
 import java.util.Locale;
 
+@Results({
 @Result(type = StreamResult.class, value = "inputStream",
-        params = {"contentType", "image/jpeg", "contentLength", "${data.length}"})
-public class GetTileAction extends DefaultAction implements ServletResponseAware {
+        params = {"contentType", "image/jpeg", "contentLength", "${data.length}"}),
+@Result(name = "fromCache", value = "304", type = HttpHeaderResult.class)
+        })
+public class GetTileAction extends DefaultAction implements ServletResponseAware, ServletRequestAware {
     public String tileName;
     private final TileFetcher tileFetcher;
     public byte[] data;
     public ByteArrayInputStream inputStream;
     private HttpServletResponse response;
+    private HttpServletRequest request;
+    private static final DateTimeFormatter HTTP_DATE_TIME_FORMATTER = DateTimeFormat.forPattern(
+            "E',' dd MMM yyyy kk:mm:ss 'GMT'").withLocale(Locale.US);
 
     public GetTileAction(final Application application, final TileFetcher tileFetcher) {
         super(application);
@@ -31,21 +42,23 @@ public class GetTileAction extends DefaultAction implements ServletResponseAware
     @SuppressWarnings({"MethodMayBeStatic"})
     @SkipValidation
     public String index() {
+        if (request.getHeader("If-None-Match") != null || request.getHeader("If-Modified-Since") != null)
+            return "fromCache";
         if (!tileName.matches("[a-zA-Z0-9.]+"))
             throw new RuntimeException("strange tilename: " + tileName);
         data = tileFetcher.fetchTile(tileName);
         inputStream = new ByteArrayInputStream(data);
-        // Wed, 09 Apr 2008 23:47:48 GMT
-        response.setHeader("Expires",
-                DateTimeFormat.forPattern("E',' dd MMM yyyy kk:mm:ss 'GMT'").withLocale(Locale.US).print(
-                        new DateTime().plusYears(1)));
-        //max-age=86400
-        //response.setHeader("Cache-Control");
+        response.setHeader("Expires", HTTP_DATE_TIME_FORMATTER.print(new DateTime().plusYears(1)));
         response.setHeader("Etag", "\"" + tileName + "-" + data.length + "\"");
+        response.setHeader("Cache-control", "max-age=31536000");
         return SUCCESS;
     }
 
     public void setServletResponse(final HttpServletResponse response) {
         this.response = response;
+    }
+
+    public void setServletRequest(final HttpServletRequest request) {
+        this.request = request;
     }
 }
