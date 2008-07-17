94c4b8b212dc17cb51a263e6b959d34c3eb76c44
nraynaud <nraynaud@d6f93abe-8f3f-0410-b366-b1a3be76d8b4>
2008-07-17 07:26:12 +0000

    - corrected date humanization

--- sport-j2ee/src/main/java/com/nraynaud/sport/web/DateHelper.java
+++ sport-j2ee/src/main/java/com/nraynaud/sport/web/DateHelper.java
@@ -1,13 +1,13 @@
 package com.nraynaud.sport.web;
 
-import org.joda.time.format.DateTimeFormatter;
-import org.joda.time.format.DateTimeFormat;
-import org.joda.time.DateTime;
 import org.joda.time.DateMidnight;
+import org.joda.time.DateTime;
 import org.joda.time.Days;
+import org.joda.time.format.DateTimeFormat;
+import org.joda.time.format.DateTimeFormatter;
 
-import java.util.Locale;
 import java.util.Date;
+import java.util.Locale;
 
 public class DateHelper {
     private DateHelper() {
@@ -21,9 +21,9 @@ public class DateHelper {
         return new DateTime(new DateMidnight());
     }
 
-    public static String humanizePastDate(final Date date, final String ...patterns) {
+    public static String humanizePastDate(final Date date, final String... patterns) {
         final DateTime dateTime = new DateTime(date);
-        final int days = Days.daysBetween(dateTime, today()).getDays();
+        final int days = Days.daysBetween(dateTime.toDateMidnight(), today()).getDays();
         final int patternLength = patterns.length;
         final String pattern = days >= 0 && days < patternLength ? patterns[days] : patterns[patternLength - 1];
         return pattern(pattern).print(dateTime);
