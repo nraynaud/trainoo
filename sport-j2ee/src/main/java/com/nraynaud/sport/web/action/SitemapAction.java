9a7c592b0afc2560b52cfae01ca5f179f35e20ab
nraynaud <nraynaud@d6f93abe-8f3f-0410-b366-b1a3be76d8b4>
2008-11-15 14:49:15 +0000

    sitemap is now public

--- sport-j2ee/src/main/java/com/nraynaud/sport/web/action/SitemapAction.java
+++ sport-j2ee/src/main/java/com/nraynaud/sport/web/action/SitemapAction.java
@@ -1,6 +1,7 @@
 package com.nraynaud.sport.web.action;
 
 import com.nraynaud.sport.Application;
+import com.nraynaud.sport.web.Public;
 import com.nraynaud.sport.web.actionsupport.DefaultAction;
 import org.apache.struts2.config.Result;
 import org.apache.struts2.config.Results;
@@ -10,6 +11,7 @@ import org.apache.struts2.dispatcher.ServletDispatcherResult;
         //the type avoids having the page decorated by application.jsp
     @Result(type = ServletDispatcherResult.class, value = "/WEB-INF/pages/sitemap.jsp")
         })
+@Public
 public class SitemapAction extends DefaultAction {
     public SitemapAction(final Application application) {
         super(application);
