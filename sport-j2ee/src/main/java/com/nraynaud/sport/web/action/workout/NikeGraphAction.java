5c1a3b475bb1cad4fea9d970c579a85917b9ec94
nraynaud <nraynaud@d6f93abe-8f3f-0410-b366-b1a3be76d8b4>
2009-03-08 17:50:12 +0000

    added url parameter to nike graph action

--- sport-j2ee/src/main/java/com/nraynaud/sport/web/action/workout/NikeGraphAction.java
+++ sport-j2ee/src/main/java/com/nraynaud/sport/web/action/workout/NikeGraphAction.java
@@ -2,22 +2,31 @@ package com.nraynaud.sport.web.action.workout;
 
 import com.nraynaud.sport.importer.Importer;
 import com.nraynaud.sport.web.Public;
+import com.nraynaud.sport.web.result.Redirect;
 import static com.opensymphony.xwork2.Action.SUCCESS;
 import org.apache.struts2.ServletActionContext;
 import org.apache.struts2.config.Result;
+import org.apache.struts2.config.Results;
 import org.apache.struts2.dispatcher.StreamResult;
 
 import java.io.ByteArrayInputStream;
 import java.io.InputStream;
 import java.net.MalformedURLException;
 import java.net.URL;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
-@Result(value = "inputStream", type = StreamResult.class, params = {"contentType", "image/png"})
+@Results({
+    @Result(value = "inputStream", type = StreamResult.class, params = {"contentType", "image/png"}),
+    @Result(name = "url", type = Redirect.class, value = "nikeGraph",
+            params = {"namespace", "/workout", "userId", "${userId}", "workoutId", "${workoutId}"})
+        })
 @Public
 public class NikeGraphAction {
     final Importer importer;
     public String userId;
     public String workoutId;
+    public String url;
 
     public NikeGraphAction(final Importer importer) {
         this.importer = importer;
@@ -25,6 +34,16 @@ public class NikeGraphAction {
 
     @SuppressWarnings({"MethodMayBeStatic"})
     public String index() {
+        if (url != null) {
+            final Pattern pattern = Pattern.compile(
+                    "http://nikeplus.nike.com/nikeplus/\\?l=runners,runs,([0-9]+),runID,([0-9]+)");
+            final Matcher matcher = pattern.matcher(url);
+            if (!matcher.matches())
+                throw new RuntimeException();
+            userId = matcher.group(1);
+            workoutId = matcher.group(2);
+            return "url";
+        }
         return SUCCESS;
     }
 
