2eca9190fd4c8d788621a661d9b89f60e276ab9c
nraynaud <nraynaud@d6f93abe-8f3f-0410-b366-b1a3be76d8b4>
2009-10-18 21:45:30 +0000

    corrected bug where tracks integration prevented creation of new workouts

--- sport-j2ee/src/main/java/com/nraynaud/sport/web/action/workout/NewAction.java
+++ sport-j2ee/src/main/java/com/nraynaud/sport/web/action/workout/NewAction.java
@@ -4,7 +4,7 @@ import com.nraynaud.sport.Application;
 import com.nraynaud.sport.web.ActionDetail;
 import com.nraynaud.sport.web.Constants;
 import com.nraynaud.sport.web.actionsupport.DefaultAction;
-import com.nraynaud.sport.web.view.WorkoutPageDetails;
+import com.nraynaud.sport.web.view.WorkoutEditPageDetails;
 import com.nraynaud.sport.web.view.WorkoutView;
 import static com.opensymphony.xwork2.Action.SUCCESS;
 import com.opensymphony.xwork2.ModelDriven;
@@ -16,7 +16,7 @@ import org.apache.struts2.config.Results;
         @Result(name = SUCCESS, value = "/WEB-INF/pages/workout/edit.jsp")
 })
 @ParentPackage(Constants.STRUTS_PACKAGE)
-public class NewAction extends DefaultAction implements ModelDriven<WorkoutPageDetails> {
+public class NewAction extends DefaultAction implements ModelDriven<WorkoutEditPageDetails> {
     public static final String PAGE_TILE = "Nouvel Entraîment";
     public static final ActionDetail CANCEL_ACTION = new ActionDetail("/workouts", "");
 
@@ -24,9 +24,9 @@ public class NewAction extends DefaultAction implements ModelDriven<WorkoutPageD
         super(application);
     }
 
-    public WorkoutPageDetails getModel() {
-        return new WorkoutPageDetails(
+    public WorkoutEditPageDetails getModel() {
+        return new WorkoutEditPageDetails(
                 new WorkoutView(null, "", "Aujourd'hui", "", "", "", "", ""), PAGE_TILE,
-                new ActionDetail("/workout", "create"), CANCEL_ACTION);
+                new ActionDetail("/workout", "create"), CANCEL_ACTION, application.fetchTracks(getUser()));
     }
 }
